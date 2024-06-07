0ad744074071e2cafd763af0321c28417d1e658e
Yeuoly <admin@srmxy.cn>
2024-06-07 22:44:24 +0800

    refine

--- src/views/chat/Editor.vue
+++ src/views/chat/Editor.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center bg-gray-900 rounded-full w-full h-12">
         <button class="text-gray-400 hover:text-gray-300 focus:outline-none w-12">
-            <Send class="w-8 text-white ml-2" />
+            <Send class="w-6 text-white ml-3" />
         </button>
         <input type="text" placeholder="Type your question" class="flex-grow bg-transparent text-white placeholder-gray-400 focus:outline-none">
         <button class="text-gray-400 hover:text-gray-300 focus:outline-none w-12">
